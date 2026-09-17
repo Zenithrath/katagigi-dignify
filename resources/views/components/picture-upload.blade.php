5b707d2eab14ff2024dfa9bcff7c5a630daa9469
dije <djibril.ranggadeja@gmail.com>
2026-09-17 14:00:19 +0800

    fix: picture upload - hide native file inputs, fix layout
    - picture.css: hide ALL file inputs via absolute offscreen (not just .picture-action)
    - picture-upload.blade.php: move input INSIDE label, add accept=image/*
    - Profile: proper centering with absolute inset-0 + flex center
    - Cover: action buttons z-20 overlay
    - Remove overflow-hidden from container

--- resources/views/components/picture-upload.blade.php
+++ resources/views/components/picture-upload.blade.php
@@ -1,49 +1,44 @@
-{{-- x-picture-upload: cover + profile picture upload widget
-     Props: $data (model with cover_picture/profile_picture), $type (create|update) --}}
 @props(['data', 'type' => 'create'])
 <div class="picture-container" x-data="pictureState({{ ($data->cover_picture ?? null) ? 'true' : 'false' }}, {{ ($data->profile_picture ?? null) ? 'true' : 'false' }})">
-    <div class="cover-picture overflow-hidden relative">
+    <div class="cover-picture">
         <img id="cover-preview" x-show="isCoverPreviewMode"
             src="{{ $type == 'update' && isset($data->cover_picture) ? url('storage/' . $data->cover_picture) : '' }}"
             alt="" srcset="" />
-        <div class="picture-action-container absolute">
-            <label>
+        <div class="picture-action-container">
+            <label class="picture-action browse cursor-pointer">
                 <input type="file" name="cover_image" id="cover_image"
-                    @change="showCoverPreview(event, 'cover-preview')" />
-                <div class="picture-action browse">
-                    <x-lucide-camera class="w-5 h-5" />
-                </div>
+                    @change="showCoverPreview(event, 'cover-preview')" accept="image/*" />
+                <x-lucide-camera class="w-5 h-5" />
             </label>
-            <button class="picture-action" @click="clearCover(event, 'cover_image', 'cover-preview')">
+            <button type="button" class="picture-action" @click="clearCover(event, 'cover_image', 'cover-preview')">
                 <x-lucide-x class="w-5 h-5" />
             </button>
         </div>
     </div>
-    <div class="profile-picture top-1/2 md:top-1/2">
+
+    <div class="profile-picture">
         <div class="relative w-full h-full">
             <img id="profile-preview" x-show="isProfilePreviewMode"
                 src="{{ $type == 'update' && isset($data->profile_picture) ? url('storage/' . $data->profile_picture) : '' }}"
-                alt="" srcset="" class="absolute w-full" />
-            <div class="absolute flex items-center justify-center w-full h-full">
-                <label>
-                    <input type="file" name="profile_image" id="profile"
-                        @change="showProfilePreview(event, 'profile-preview')" />
-                    <div class="picture-action browse">
-                        <x-lucide-camera class="w-5 h-5" />
-                    </div>
-                </label>
-            </div>
+                alt="" srcset="" class="absolute w-full h-full object-cover" />
+            <label class="absolute inset-0 flex items-center justify-center cursor-pointer z-10">
+                <input type="file" name="profile_image" id="profile"
+                    @change="showProfilePreview(event, 'profile-preview')" accept="image/*" />
+                <div class="picture-action browse">
+                    <x-lucide-camera class="w-5 h-5" />
+                </div>
+            </label>
         </div>
     </div>
 
     @error('cover_image')
-        <div class="ml-36 md:ml-48">
+        <div class="ml-36 md:ml-48 mt-1">
             <small class="danger">{{ $message }}</small>
         </div>
     @enderror
 
     @error('profile_image')
-        <div class="ml-36 md:ml-48">
+        <div class="ml-36 md:ml-48 mt-1">
             <small class="danger">{{ $message }}</small>
         </div>
     @enderror
